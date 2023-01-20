0096e900de423b778bdca50ef3e6b831227c1a52
Fleetwood <wizening@gmail.com>
2023-01-20 17:01:24 -0600

    Removed updateAll from api

--- pages/api/updateAll.ts
+++
@@ -1,9 +0,0 @@
-import { updateAll } from "@/prisma/PrismaContext"
-import { NextApiRequest, NextApiResponse } from "next"
-
-const handle = async (req: NextApiRequest, res: NextApiResponse) => {
-    const update = await updateAll()
-    res.status(200).json(update)
-}
-
-export default handle
