969021b9cc61f98b6f2de858bf37dbb1158bb811
Fleetwood <wizening@gmail.com>
2023-01-20 17:12:44 -0600

    Cleaned up CommandUI and prettydid scrollbar

--- components/ConversionCommandUI.tsx
+++ components/ConversionCommandUI.tsx
@@ -1,41 +1,56 @@
-import React, { useEffect, useState } from "react"
-import useApiQuery from "@/hooks/useApiQuery"
-import { ConversionCommand, ConversionModule, ConversionFile } from ".prisma/client"
+import React, { useEffect, useState } from "react";
+import useApiQuery from "@/hooks/useApiQuery";
+import {
+  ConversionCommand,
+  ConversionModule,
+  ConversionFile,
+} from ".prisma/client";
 
 type ConversionFileProps = {
-  command: ConversionCommand
-  file: ConversionFile
-  module: ConversionModule
-  onCommandUpdate: Function
-}
+  command: ConversionCommand;
+  file: ConversionFile;
+  module: ConversionModule;
+  onCommandUpdate: Function;
+};
 
-const ConversionCommandUI = ({command, file, module, onCommandUpdate}: ConversionFileProps) => {
-  const { completeCommand, invalidateQuery } = useApiQuery()
-  const [status, setStatus] = useState('bg-accent')
+const ConversionCommandUI = ({
+  command,
+  file,
+  module,
+  onCommandUpdate,
+}: ConversionFileProps) => {
+  const { completeCommand, invalidateQuery } = useApiQuery();
+  const [status, setStatus] = useState("bg-accent");
 
   useEffect(() => {
-    setStatus(command.complete ? 'bg-success' : 'bg-accent')
-  }, [command])
+    setStatus(command.complete ? "bg-success" : "bg-accent");
+  }, [command]);
 
   const handleComplete = async () => {
-    console.log(`ConversionCommandUI handleComplete`)
-    await completeCommand(command.id, !(command.complete))
+    console.log(`ConversionCommandUI handleComplete`);
+    await completeCommand(command.id, !command.complete)
       .then(async () => {
-        console.log(`\tConversionCommandUI handleCompleted`)
-        onCommandUpdate()
+        console.log(`\tConversionCommandUI handleCompleted`);
+        onCommandUpdate();
       })
-      .then(() => setStatus(current => !(command.complete) ? 'bg-success' : 'bg-accent'))
-      .catch((e) => 
-        console.log(`\tConversionCommandUI update FAIL ${e.message}`)
+      .then(() =>
+        setStatus((current) => (!command.complete ? "bg-success" : "bg-accent"))
       )
-  }
+      .catch((e) =>
+        console.log(`\tConversionCommandUI update FAIL ${e.message}`)
+      );
+  };
 
   return (
-    <div className={`flex ${status} even:bg-opacity-20 odd:bg-opacity-30 my-2 rounded-full`} key={command.id}>
-      <div onClick={(e) => {
+    <div
+      className={`flex ${status} even:bg-opacity-20 odd:bg-opacity-30 my-2 rounded-full`}
+      key={command.id}
+    >
+      <div
+        onClick={(e) => {
           // e.preventDefault
           // e.bubbles = false
-          handleComplete()
+          handleComplete();
         }}
       >
         {command.complete ? (
@@ -48,14 +63,23 @@ const ConversionCommandUI = ({command, file, module, onCommandUpdate}: Conversio
           </div>
         )}
       </div>
-      <div className={`text-sm ${command.complete ? 'text-success-content':'text-accent-content'} px-2 overflow-x-hidden font-mono flex space-x-4`}>
-        <div>{module.id}</div>
-        <div>{file.id}</div>
-        <div>{command.complete}</div>
-        <pre>{JSON.stringify(command,null,2)}</pre>
+      <div
+        className={`text-sm ${
+          command.complete ? "text-success-content" : "text-content"
+        } px-2 overflow-x-hidden font-mono flex space-x-4`}
+      >
+        <pre
+          className={`overflow-x-auto scrollbar scrollbar-track-transparent scrollbar-rounded-full scrollbar-h-2 ${
+            command.complete
+              ? "scrollbar-thumb-success"
+              : "scrollbar-thumb-info"
+          }`}
+        >
+          {command.code}
+        </pre>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ConversionCommandUI
+export default ConversionCommandUI;
